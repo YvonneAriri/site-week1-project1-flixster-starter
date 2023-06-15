89c47c7642e50d28b645b80b465ee7778c8f6ae8
Yvonne Ariri <yvonneariri@meta.com>
2023-06-14 21:49:53 -0700

    [Flixster] Implement functionality to show movie list in page UI

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
-https://api.themoviedb.org/3/search/movie?api_key=https://api.themoviedb.org/3/search/movie?api_key=1659706dfae4865fb5606d1db53414cd&q="hello"
-gifsDiv.addEventListener("click", async (event) => {
-    event.preventDefault(); // Prevent form submission (optional)
+// Assign movie grid HTML element to variable
+const movieGrid = document.getElementById("movie-grid");
 
-    // http://api.giphy.com/v1/channels/search?api_key=9yiUnV6M2o6MsLcoAxcaDWglijaCuBG7&q="hello"
+/**
+ * Get list of movies now playing in theatres by making a fetch request to moviedb API
+ * with assigned API key
+ */
+const fetchAndShowMovies = async () => {
+  try {
+    const API_KEY = "1659706dfae4865fb5606d1db53414cd";
+    const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&page=1`;
 
-    try {
-        const apiKey = "1659706dfae4865fb5606d1db53414cd";
-        const searchTerm = searchInput.value;
-        const url = `https://api.themoviedb.org/search/movie?api_key=${apiKey}&q=${encodeURIComponent(searchTerm)}`;
-    
-        const response = await fetch(url);
-        const data = await response.json();
-        const images = data.data;
+    const response = await fetch(url);
+    const data = await response.json();
+    const images = data.results;
 
-        images.forEach(gif => {
-            const img = document.createElement("img");
-            img.src = gif.images.original.url;
-            gifsDiv.appendChild(img); // Step 3: Add the image url to the gifsDiv
-        });
+    images.forEach((movie) => {
+      // Create div element wrapper for each movie with a class name of "movie-card"
+      const movieCard = document.createElement("div");
+      movieCard.classList.add("movie-card");
+      // Create img element for each movie with a class name of "movie-poster"
+      const img = document.createElement("img");
+      img.classList.add("movie-poster");
+      img.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+      // Append movie image to movieCard div element
+      movieCard.appendChild(img);
+      // Append movie card to movieGrid div element
+      movieGrid.appendChild(movieCard);
+    });
+  } catch (error) {
+    // Handle any errors that occur during the fetch request
+    console.log(error);
+  }
+};
 
-        console.log(data.data[0]);
-        console.log(data.data[0].images.original.url);
-        // Process the data returned from the API
-        // console.log(data);
-    } catch (error) {
-        // Handle any errors that occur during the fetch request
-        console.log(error);
-    }
-
-    // Perform your desired action here
-    
+/**
+ * Immediately the page loads, we want to call the fetchAndShowMovies function so the movies
+ * can be shown on the page UI
+ */
+window.addEventListener("load", (event) => {
+  event.preventDefault();
+  fetchAndShowMovies();
 });
-
